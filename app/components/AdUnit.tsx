c5a3502f6d64796f8ef35273922ee4812cb68dd4
tangtao <1730462119@qq.com>
2025-11-11 13:07:58 +0800

    修改广告位高度

--- app/components/AdUnit.tsx
+++ app/components/AdUnit.tsx
@@ -10,13 +10,15 @@ export const AdUnit = ({ AdId,slot, className }: AdUnitProps) => {
         <div className={`hidden xl:block sticky top-20 h-min ${className}`}>
             <div
                 className="bg-gray-100 text-gray-500 flex items-center justify-center border border-dashed border-gray-300 rounded-lg text-sm font-mono p-1"
-                style={{ width: '160px', height: '300px', fontSize: '10px' }}
+                style={{ width: 'auto', height: '450px', fontSize: '10px' }}
             >
                 <ins
                     className="adsbygoogle"
                     style={{ display: 'inline-block', width: '100%', height: '100%' }}
                     data-ad-client={AdId}
                     data-ad-slot={slot}
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"
                 ></ins>
             </div>
         </div>
