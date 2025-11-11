c11b55c470be2f9a53bf368b827f78ab8032dcbe
tangtao <1730462119@qq.com>
2025-11-11 09:10:56 +0800

    添加真实广告id

--- app/components/AdUnit.tsx
+++ app/components/AdUnit.tsx
@@ -1,12 +1,13 @@
+
 interface AdUnitProps {
     AdId:String;
     slot: string;
     className?: string;
 }
 
-export const AdUnit = ({ AdId, slot }: { AdId: string, slot: string }) => {
+export const AdUnit = ({ AdId,slot, className }: AdUnitProps) => {
     return (
-        <div className="hidden xl:block sticky top-20 h-min">
+        <div className={`hidden xl:block sticky top-20 h-min ${className}`}>
             <div
                 className="bg-gray-100 text-gray-500 flex items-center justify-center border border-dashed border-gray-300 rounded-lg text-sm font-mono p-1"
                 style={{ width: '160px', height: '300px', fontSize: '10px' }}
@@ -16,9 +17,7 @@ export const AdUnit = ({ AdId, slot }: { AdId: string, slot: string }) => {
                     style={{ display: 'inline-block', width: '100%', height: '100%' }}
                     data-ad-client={AdId}
                     data-ad-slot={slot}
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"
-                />
+                ></ins>
             </div>
         </div>
     );
